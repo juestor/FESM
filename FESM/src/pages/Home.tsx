70f7dbf4c562e31c166a4883f022aa3ea20daf47
David Alejandro Torres Solano <wacadaja@MacBook-Pro-de-David.local>
2020-06-03 21:43:59 -0500

    Primera prueba

--- FESM/src/pages/Home.tsx
+++ FESM/src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>FESM</IonTitle>
+          <IonTitle>MENU FESM</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
