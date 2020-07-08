7edfcd88bc9eb69f216d1aee877bb54789f7bcfa
juestor <jtorres@psl.com.co>
2020-07-07 21:12:21 -0500

    creamos la imagen de informacion, y creamos un state para el componente de New Participant

--- FESM/src/components/ImportantAdvice/ImportantAdvice.tsx
+++ FESM/src/components/ImportantAdvice/ImportantAdvice.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import './ImportantAdvice.css';
-import { IonList, IonItem, IonLabel } from '@ionic/react';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImg } from '@ionic/react';
 
 interface ContainerProps { }
 
 const ImportantAdvice: React.FC<ContainerProps> = () => {
   
   return (
-    <div className="container"> 
-      <IonList>
-        <IonItem class="title-item">
-          <IonLabel class="advisement-title">Aviso Importante</IonLabel><br />
-        </IonItem> 
-        <IonItem class="title-item">
-          <IonLabel class="advisement">El objetivo de esta apicación es determinar el nivel del fitness en personas sanas, no atletas, mayores de 18 años, debe ser realizada por personal idóneo (médicos del deporte)</IonLabel>
-        </IonItem> 
-      </IonList> 
+    <div className="container">
+      <IonCard>
+        <IonImg src="assets/img/jogging.jpg" />
+        <IonCardHeader>
+          <IonCardTitle class="card-title">Aviso Importante</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+        El objetivo de esta apicación es determinar el nivel del fitness en personas sanas, no atletas, mayores de 18 años, debe ser realizada por personal idóneo (médicos del deporte).
+        </IonCardContent>
+      </IonCard> 
     </div>
   );
 };
--- FESM/src/components/NewParticipant/NewParticipant.tsx
+++ FESM/src/components/NewParticipant/NewParticipant.tsx
@@ -1,82 +1,109 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { IonList, IonItem, IonInput, IonLabel, IonDatetime, IonSegment, IonSegmentButton, IonButton } from '@ionic/react';
 
 import './NewParticipant.css';
 
-interface ContainerProps { }
+class NewParticipant extends Component <{},any> {
 
-const NewParticipant: React.FC<ContainerProps> = () => {
-  const [name, setName] = useState<string>();
-  const [lastName, setLastName] = useState<string>();
-  const [birthDate, setBirthDate] = useState<string>();
-  const [weight, setWeight] = useState<number>();
-  const [height, setHeight] = useState<number>();
-  const [gender, setGender] = useState<string>();
+  constructor(props: any) {
+    super(props)
 
-  // const [birthDate, setBirthDate] = useState<string>('2012-12-15T13:47:20.789');
-
-  const createParticipant = () => {
-    console.log('Se va a crear el participante:');
-    console.log('name', name);
-    console.log('lastName', lastName);
-    console.log('birthDate', birthDate);
-    console.log('weight', weight);
-    console.log('height', height);
-    console.log('gender', gender);
+    this.state = {
+      name: '',
+      lastName: '',
+      birthDate: '',
+      weight: 0,
+      height: 0,
+      gender: ''
+    }
   }
 
-  return (
-    <div className="container">    
-      <IonList>
-        <IonItem class="title-item">
-          <IonLabel class="title-label" color="primary">Nuevo Participante</IonLabel><br />
-        </IonItem>
-        <IonItem>
-          <IonLabel position="floating">Nombre</IonLabel>
-          <IonInput value={name} onIonChange={e => setName(e.detail.value!)}></IonInput>
-        </IonItem>
+  setNameValue = (nameText: string) => {
+    this.setState({ name: nameText })
+  }
+  setLastNameValue = (lastNameText: string) => {
+    this.setState({ lastName: lastNameText })
+  }
+  setBirthDateValue = (birthDate: string) => {
+    this.setState({ birthDate: birthDate })
+  }
+  setWeightValue = (weightText: number) => {
+    this.setState({ weight: weightText })
+  }
+  setHeightValue = (heightText: number) => {
+    this.setState({ height: heightText })
+  }
+  setGenderValue = (genderText: string | undefined) => {
+    this.setState({ gender: genderText })
+  }
 
-        <IonItem>
-          <IonLabel position="floating">Apellido</IonLabel>
-          <IonInput value={lastName} onIonChange={e => setLastName(e.detail.value!)}></IonInput>
-        </IonItem>
+  createParticipant = () => {
+    console.log('Se va a crear el participante:');
+    console.log(this.state);
+  }
 
-        <IonItem>
-          <IonLabel position="floating">Fecha de nacimiento</IonLabel>
-          <IonDatetime displayFormat="MM/DD/YYYY" min="1900-01-01" max="2100-12-31" value={birthDate} onIonChange={e => setBirthDate(e.detail.value!)}></IonDatetime>
-        </IonItem>
-
-        <IonItem>
-          <IonLabel position="floating">Peso (Kg)</IonLabel>
-          <IonInput type="number" value={weight} onIonChange={e => setWeight(parseFloat(e.detail.value!))}></IonInput>
-        </IonItem>
-
-        <IonItem>
-          <IonLabel position="floating">Altura (cm)</IonLabel>
-          <IonInput type="number" value={height} onIonChange={e => setHeight(parseFloat(e.detail.value!))}></IonInput>
-        </IonItem>
-
-        <IonItem>
-          <IonLabel position="fixed">Sexo</IonLabel>
-        </IonItem>
-
-        <IonItem>
-          <IonSegment onIonChange={e => setGender(e.detail.value)}>
-            <IonSegmentButton value='female'>
-              <IonLabel>Femenino</IonLabel>
-            </IonSegmentButton>
-            <IonSegmentButton value='male'>
-              <IonLabel>Masculino</IonLabel>
-            </IonSegmentButton>
-          </IonSegment>
-        </IonItem>
-
-        <IonItem>
-          <IonButton class="create-button" expand="full" onClick={createParticipant}>Crear</IonButton>
-        </IonItem>
-      </IonList>
-    </div>
-  );
+  render() {
+    const {
+      name, 
+      lastName,
+      birthDate,
+      weight,
+      height,
+      gender
+    } = this.state;
+    return (
+      <div className="container">    
+        <IonList>
+          <IonItem class="title-item">
+            <IonLabel class="title-label" color="primary">Nuevo Participante</IonLabel><br />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Nombre</IonLabel>
+            <IonInput value={name} onIonChange={e => this.setNameValue(e.detail.value!)}></IonInput>
+          </IonItem>
+  
+          <IonItem>
+            <IonLabel position="floating">Apellido</IonLabel>
+            <IonInput value={lastName} onIonChange={e => this.setLastNameValue(e.detail.value!)}></IonInput>
+          </IonItem>
+  
+          <IonItem>
+            <IonLabel position="floating">Fecha de nacimiento</IonLabel>
+            <IonDatetime displayFormat="MM/DD/YYYY" min="1900-01-01" max="2100-12-31" value={birthDate} onIonChange={e => this.setBirthDateValue(e.detail.value!)}></IonDatetime>
+          </IonItem>
+  
+          <IonItem>
+            <IonLabel position="floating">Peso (Kg)</IonLabel>
+            <IonInput type="number" value={weight} onIonChange={e => this.setWeightValue(parseFloat(e.detail.value!))}></IonInput>
+          </IonItem>
+  
+          <IonItem>
+            <IonLabel position="floating">Altura (cm)</IonLabel>
+            <IonInput type="number" value={height} onIonChange={e => this.setHeightValue(parseFloat(e.detail.value!))}></IonInput>
+          </IonItem>
+  
+          <IonItem>
+            <IonLabel position="fixed">Sexo</IonLabel>
+          </IonItem>
+  
+          <IonItem>
+            <IonSegment value={gender} onIonChange={e => this.setGenderValue(e.detail.value)}>
+              <IonSegmentButton value='female'>
+                <IonLabel>Femenino</IonLabel>
+              </IonSegmentButton>
+              <IonSegmentButton value='male'>
+                <IonLabel>Masculino</IonLabel>
+              </IonSegmentButton>
+            </IonSegment>
+          </IonItem>
+  
+          <IonItem>
+            <IonButton class="create-button" expand="full" onClick={this.createParticipant}>Crear</IonButton>
+          </IonItem>
+        </IonList>
+      </div>
+    );
+  }
 };
 
 export default NewParticipant;
