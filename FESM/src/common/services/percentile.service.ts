fe1ffab147ef7f183871cb0011422958684454b1
juestor <jtorres@psl.com.co>
2020-08-31 20:30:23 -0500

    funciones calculo percentil

--- FESM/src/common/services/percentile.service.ts
+++ FESM/src/common/services/percentile.service.ts
@@ -1,37 +1,99 @@
-import { GreasyPercentilesValues } from '../percentiles/greasy';
 import { MeasurePercentiles, AgeRange, Percentile } from '../model';
 
+import { AbsPercentilesValues } from '../percentiles/abs';
+import { GreasyPercentilesValues } from '../percentiles/greasy';
+import { PushUpsPercentilesValues } from '../percentiles/push-ups';
+import { SitNReachPercentilesValues } from '../percentiles/sit-n-reach';
+import { ChestPressPercentilesValues } from '../percentiles/chest-press';
+import { LegPressPercentilesValues } from '../percentiles/leg-press';
+import { Vo2PercentilesValues } from '../percentiles/vo2';
 
-const PercentileService = {
-  getGreasyPercentile: (value: number, age: number, gender: string): number => {
-    const genderPercentiles = GreasyPercentilesValues
-      .find((percentile: MeasurePercentiles) => percentile.gender === gender);
+const getGenderPercentiles = (gender: string, measurePercentiles: MeasurePercentiles[]): AgeRange[] => {
+  const result = measurePercentiles
+    .find((percentile: MeasurePercentiles) => percentile.gender === gender);
+  return result ? result.ageRange : [];
+};
 
-    const ageRangePercentiles = genderPercentiles?.ageRange
-      .find((ageRange: AgeRange) => age >= ageRange.lower && age <= ageRange.higher);
+const getPercentiles = (age: number, ageRanges: AgeRange[]): Percentile[] => {
+  const result = ageRanges
+    .find((ageRange: AgeRange) => age >= ageRange.lower && age <= ageRange.higher);
+  return result ? result.percentiles : [];
+}
 
-    let minValue: number;
-    let result: number = 0;
+const getPercentile = (value: number, percentiles: Percentile[]) : number=> {
+  let minValue: number;
+  let result: number = 0;
 
-    ageRangePercentiles?.percentiles
-      .forEach((percentile: Percentile) => {
-        const minDiff = value - percentile.value;
-        const modMinDif = (minDiff < 0) ? (minDiff * -1) : minDiff;
+  percentiles
+    .forEach((percentile: Percentile) => {
+      const minDiff = value - percentile.value;
+      const modMinDif = (minDiff < 0) ? (minDiff * -1) : minDiff;
 
-        if (minValue) {
-          if (modMinDif < minValue) {
-            minValue = modMinDif;
-            result = percentile.percentile;
-          }
-        } else {
+      if (minValue) {
+        if (modMinDif < minValue) {
           minValue = modMinDif;
           result = percentile.percentile;
         }
+      } else {
+        minValue = modMinDif;
+        result = percentile.percentile;
       }
-    );
+    }
+  );
 
-    return result;
-  }
+  return result;
+}
+
+const PercentileService = {
+  getGreasyPercentile: (value: number, age: number, gender: string): number => {
+    const genderPercentiles = getGenderPercentiles(gender, GreasyPercentilesValues);
+
+    const ageRangePercentiles = getPercentiles(age, genderPercentiles);
+
+    return getPercentile(value, ageRangePercentiles);
+  },
+  getAbsPercentile: (value: number, age: number, gender: string): number => {
+    const genderPercentiles = getGenderPercentiles(gender, AbsPercentilesValues);
+
+    const ageRangePercentiles = getPercentiles(age, genderPercentiles);
+
+    return getPercentile(value, ageRangePercentiles);
+  },
+  getPushUpsPercentile: (value: number, age: number, gender: string): number => {
+    const genderPercentiles = getGenderPercentiles(gender, PushUpsPercentilesValues);
+
+    const ageRangePercentiles = getPercentiles(age, genderPercentiles);
+
+    return getPercentile(value, ageRangePercentiles);
+  },
+  getSitNReachPercentile: (value: number, age: number, gender: string): number => {
+    const genderPercentiles = getGenderPercentiles(gender, SitNReachPercentilesValues);
+
+    const ageRangePercentiles = getPercentiles(age, genderPercentiles);
+
+    return getPercentile(value, ageRangePercentiles);
+  },
+  getChestPressPercentile: (value: number, age: number, gender: string): number => {
+    const genderPercentiles = getGenderPercentiles(gender, ChestPressPercentilesValues);
+
+    const ageRangePercentiles = getPercentiles(age, genderPercentiles);
+
+    return getPercentile(value, ageRangePercentiles);
+  },
+  getLegPressPercentile: (value: number, age: number, gender: string): number => {
+    const genderPercentiles = getGenderPercentiles(gender, LegPressPercentilesValues);
+
+    const ageRangePercentiles = getPercentiles(age, genderPercentiles);
+
+    return getPercentile(value, ageRangePercentiles);
+  },
+  getVo2Percentile: (value: number, age: number, gender: string): number => {
+    const genderPercentiles = getGenderPercentiles(gender, Vo2PercentilesValues);
+
+    const ageRangePercentiles = getPercentiles(age, genderPercentiles);
+
+    return getPercentile(value, ageRangePercentiles);
+  },
 }
 
 export default PercentileService;
